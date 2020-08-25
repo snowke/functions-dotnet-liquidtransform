4d49e02ea7055bab6126e307d2277f3d5c8b69d0
Aaron.Kimaa <Aaron.Kim@WSTLPTP10525d>
2020-08-25 14:39:12 +0800

    Fixed codes to work with the latest core 3.1(needs to be async all the way) & json parsing of input to be correctly transfored to hash

--- LiquidTransform.functionapp.v3/LiquidTransformer.cs
+++ LiquidTransform.functionapp.v3/LiquidTransformer.cs
@@ -5,13 +5,39 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using DotLiquid;
 using System.Text;
 using System;
 using Microsoft.Extensions.Logging;
+using DotLiquid;
+using Newtonsoft.Json.Linq;
 
 namespace LiquidTransform.functionapp.v2
 {
+    public static class JsonHelper
+    {
+        // this is a replace of JObject.Parce() because some errors when dotliquid try to render template
+        public static object Deserialize(string json)
+        {
+            return ToObject(JToken.Parse(json));
+        }
+
+        private static object ToObject(JToken token)
+        {
+            switch (token.Type)
+            {
+                case JTokenType.Object:
+                    return token.Children<JProperty>()
+                        .ToDictionary(prop => prop.Name,
+                                      prop => ToObject(prop.Value));
+
+                case JTokenType.Array:
+                    return token.Select(ToObject).ToList();
+
+                default:
+                    return ((JValue)token).Value;
+            }
+        }
+    }
     public static class LiquidTransformer
     {
         /// <summary>
@@ -28,12 +54,16 @@ namespace LiquidTransform.functionapp.v2
             [Blob("liquid-transforms/{liquidtransformfilename}", FileAccess.Read)] Stream inputBlob,
             ILogger log)
         {
+            HttpResponseMessage responseMessage = null;
+
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             if (inputBlob == null)
             {
-                log.LogError("inputBlog null");
-                return req.CreateErrorResponse(HttpStatusCode.NotFound, "Liquid transform not found");
+                log.LogError("inputBlob null");
+
+                responseMessage = new HttpResponseMessage(HttpStatusCode.NotFound);
+                responseMessage.Content = new StringContent("Liquid transform not found");
             }
 
             // This indicates the response content type. If set to application/json it will perform additional formatting
@@ -41,31 +71,37 @@ namespace LiquidTransform.functionapp.v2
             string requestContentType = req.Content.Headers.ContentType.MediaType;
             string responseContentType = req.Headers.Accept.FirstOrDefault().MediaType;
 
-            // Load the Liquid transform in a string
-            var sr = new StreamReader(inputBlob);
-            var liquidTransform = sr.ReadToEnd();
+            // Load the Liquid transform in a string\
+            string liquidTransform = string.Empty;
+            using(StreamReader sr = new StreamReader(inputBlob))
+            {
+                liquidTransform = await sr.ReadToEndAsync();
+            }
 
             var contentReader = ContentFactory.GetContentReader(requestContentType);
             var contentWriter = ContentFactory.GetContentWriter(responseContentType);
 
-            Hash inputHash;
+            Hash inputHash = null;
 
             try
             {
-                inputHash = await contentReader.ParseRequestAsync(req.Content);
-
+                var json = await req.Content.ReadAsStringAsync();
+                
+                inputHash = Hash.FromDictionary((dynamic)JsonHelper.Deserialize(json));
             }
             catch (Exception ex)
             {
                 log.LogError(ex.Message, ex);
-                return req.CreateErrorResponse(HttpStatusCode.InternalServerError, "Error parsing request body", ex);
+                
+                responseMessage = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+                responseMessage.Content = new StringContent("Error parsing request body");
             }
 
             // Register the Liquid custom filter extensions
             Template.RegisterFilter(typeof(CustomFilters));
 
             // Execute the Liquid transform
-            Template template;
+            Template template = null;
 
             try
             {
@@ -74,7 +110,9 @@ namespace LiquidTransform.functionapp.v2
             catch (Exception ex)
             {
                 log.LogError(ex.Message, ex);
-                return req.CreateErrorResponse(HttpStatusCode.InternalServerError, "Error parsing Liquid template", ex);
+
+                responseMessage = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+                responseMessage.Content = new StringContent("Error parsing Liquid template");
             }
 
             string output = string.Empty;
@@ -82,44 +120,53 @@ namespace LiquidTransform.functionapp.v2
             try
             {
                 output = template.Render(inputHash);
+                log.LogInformation(output);
             }
             catch (Exception ex)
             {
                 log.LogError(ex.Message, ex);
-                return req.CreateErrorResponse(HttpStatusCode.InternalServerError, "Error rendering Liquid template", ex);
+
+                responseMessage = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+                responseMessage.Content = new StringContent("Error rendering Liquid template");
             }
 
             if (template.Errors != null && template.Errors.Count > 0)
             {
                 if (template.Errors[0].InnerException != null)
                 {
-                    return req.CreateErrorResponse(HttpStatusCode.InternalServerError, $"Error rendering Liquid template: {template.Errors[0].Message}", template.Errors[0].InnerException);
+                    log.LogError(template.Errors[0].InnerException, "Error rendering Liquid template: {error}", template.Errors[0].Message);
+
+                    responseMessage = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+                    responseMessage.Content = new StringContent($"Error rendering Liquid template: {template.Errors[0].Message}");
                 }
                 else
                 {
-                    return req.CreateErrorResponse(HttpStatusCode.InternalServerError, $"Error rendering Liquid template: {template.Errors[0].Message}");
+                    log.LogError("Error rendering Liquid template: {error}", template.Errors[0].Message);
+
+                    responseMessage = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+                    responseMessage.Content = new StringContent($"Error rendering Liquid template: {template.Errors[0].Message}");
+                }
+            }
+            else
+            {
+                try
+                {
+                    var content = contentWriter.CreateResponse(output);
+
+                    responseMessage = new HttpResponseMessage(HttpStatusCode.OK);
+                    responseMessage.Content = content;
+                }
+                catch (Exception ex)
+                {
+                    // Just log the error, and return the Liquid output without parsing
+                    log.LogError(ex.Message, ex);
+
+                    responseMessage = new HttpResponseMessage(HttpStatusCode.OK);
+                    responseMessage.Content = new StringContent(output, Encoding.UTF8, responseContentType);
                 }
             }
 
-            try
-            {
-                var content = contentWriter.CreateResponse(output);
-
-                return new HttpResponseMessage(HttpStatusCode.OK)
-                {
-                    Content = content
-                };
-            }
-            catch (Exception ex)
-            {
-                // Just log the error, and return the Liquid output without parsing
-                log.LogError(ex.Message, ex);
-
-                return new HttpResponseMessage(HttpStatusCode.OK)
-                {
-                    Content = new StringContent(output, Encoding.UTF8, responseContentType)
-                };
-            }
+            return responseMessage;
         }
     }
 }
